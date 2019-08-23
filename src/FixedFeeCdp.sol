cbcb9a68adbe51adbf26c17ae3bd8dd3e38fb8aa
andy8052 <andychorlian@gmail.com>
2019-08-23 22:36:26 +0200

    starting to add stuff

--- src/FixedFeeCdp.sol
+++ src/FixedFeeCdp.sol
@@ -1,4 +1,36 @@
 pragma solidity ^0.5.10;
 
+contract ProxyOwner {
+    address owner;
+    address lender;
+
+    modifier auth() {
+        require(
+            msg.sender == owner || msg.sender == lender,
+            "Only owner or lender can call this."
+        );
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            "Only the owner can call this."
+        );
+        _;
+    }
+
+    modifier onlyLender() {
+        require(
+            msg.sender == lender,
+            "Only the lender can call this."
+        );
+        _;
+    }
+}
+
 contract FixedFeeCdp {
+
+
+
 }
